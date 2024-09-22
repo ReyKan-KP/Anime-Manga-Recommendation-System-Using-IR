8ff179d257556c4bcf8cf495959384a450f44bf6
Kanishaka Pranjal <97291226+ReyKan-KP@users.noreply.github.com>
2024-09-22 06:58:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ def process_feedback_input():
 
     return jsonify(success=True, message="<h2>Feedback processed successfully")
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
